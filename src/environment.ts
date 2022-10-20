5d81e3a4c734bb0eb1f21d2598904e3d106fa72c
Rick van Dam <Barsonax@users.noreply.github.com>
2022-10-20 19:02:30 +0200

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,6 @@
 const environment = {
   URL: process.env.CF_PAGES == 1 ?
-    process.env.CONTEXT == 'production' ?
       process.env.CF_PAGES_URL :
-      process.env.DEPLOY_PRIME_URL :
     'http://localhost:8000'
 }
 export default environment
