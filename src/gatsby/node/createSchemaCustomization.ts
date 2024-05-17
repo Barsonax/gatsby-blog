e06b67baf26c788489f8b99aa3474aeea6caf7f5
Rick van Dam <rick-van-dam@outlook.com>
2024-05-17 22:27:49 +0200

    trigger change

--- src/gatsby/node/createSchemaCustomization.ts
+++ src/gatsby/node/createSchemaCustomization.ts
@@ -90,6 +90,7 @@ export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] 
           return null;
         }
 
+
         console.log(`Resolved featureImage path ${partialPath} to ${fileNode.absolutePath} for context ${context.path}`)
         return fileNode
       }
